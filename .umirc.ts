626c42def05be918a7a02dec05ba08198de487a8
余柳 <yuliu@tianan-cyber.com>
2020-05-12 16:32:28 +0800

    init

---
+++ .umirc.ts
@@ -0,0 +1,40 @@
+import { IConfig } from 'umi-types';
+
+// ref: https://umijs.org/config/
+const config: IConfig =  {
+  treeShaking: true,
+  routes: [
+    {
+      path: '/',
+      component: '../layouts/index',
+      routes: [
+        { path: '/', component: '../pages/index' }
+      ]
+    }
+  ],
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    ['umi-plugin-react', {
+      antd: true,
+      dva: {
+        immer:true
+      },
+      dynamicImport: { webpackChunkName: true },
+      title: 'umi-dva',
+      dll: true,
+      
+      routes: {
+        exclude: [
+          /models\//,
+          /services\//,
+          /model\.(t|j)sx?$/,
+          /service\.(t|j)sx?$/,
+          /components\//,
+        ],
+      },
+    }],
+  ],
+  ssr: true, 
+}
+
+export default config;
